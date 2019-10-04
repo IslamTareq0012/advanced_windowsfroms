9d66a63af43142f76774641aaa8274df3975d256
IslamTareq0012 <31237314+IslamTareq0012@users.noreply.github.com>
2019-10-05 00:38:33 +0200

    soccer is setted

--- Drawing/Program.cs
+++ Drawing/Program.cs
@@ -16,7 +16,7 @@ namespace Drawing
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new DrawingLines());
+            Application.Run(new Soccer());
         }
     }
 }
---
+++ Drawing/Soccer.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Drawing
+{
+    partial class Soccer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Soccer));
+            this.man1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.man1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // man1
+            // 
+            this.man1.Image = ((System.Drawing.Image)(resources.GetObject("man1.Image")));
+            this.man1.Location = new System.Drawing.Point(21, 37);
+            this.man1.Name = "man1";
+            this.man1.Size = new System.Drawing.Size(204, 377);
+            this.man1.TabIndex = 0;
+            this.man1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(516, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(205, 377);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Soccer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(752, 447);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.man1);
+            this.Name = "Soccer";
+            this.Text = "Soccer";
+            ((System.ComponentModel.ISupportInitialize)(this.man1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox man1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
